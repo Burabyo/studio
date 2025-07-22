4699cf2616464d110d3d727b972fe1da059575bb
Burabyo Gift <burabyogift1@gmail.com>
2025-07-22 12:49:44 +0000

    [2025-07-22T12:34:37.986Z]  @firebase/firestore: Firestore (11.10.0): en

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,6 +1,6 @@
 
 import { initializeApp, getApps, getApp } from "firebase/app";
-import { getFirestore, enableIndexedDbPersistence } from "firebase/firestore";
+import { initializeFirestore, memoryLocalCache, persistentLocalCache } from "firebase/firestore";
 import { getFunctions, connectFunctionsEmulator } from "firebase/functions";
 import { getAuth } from "firebase/auth";
 
@@ -15,7 +15,12 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-const db = getFirestore(app);
+
+// Initialize Firestore with offline persistence
+const db = initializeFirestore(app, {
+  localCache: persistentLocalCache(/*settings*/),
+});
+
 const auth = getAuth(app);
 const functions = getFunctions(app);
 
@@ -31,19 +36,5 @@ if (process.env.NODE_ENV === 'development') {
     }
 }
 
-
-// Enable offline persistence if running in the browser
-if (typeof window !== 'undefined') {
-  enableIndexedDbPersistence(db)
-    .catch((err) => {
-      if (err.code == 'failed-precondition') {
-        // Multiple tabs open, persistence can only be enabled in one tab at a time.
-        console.warn('Firestore persistence failed: multiple tabs open.');
-      } else if (err.code == 'unimplemented') {
-        // The current browser does not support all of the features required to enable persistence
-        console.warn('Firestore persistence not available in this browser.');
-      }
-    });
-}
 
 export { app, db, auth, functions };
